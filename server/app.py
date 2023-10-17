4213578e743b648955d9fa9c471cb00eb1c80e51
+ <+>
2023-10-17 12:45:13 +0300

    completed lab

--- server/app.py
+++ server/app.py
@@ -1,6 +1,8 @@
 from flask import Flask, request, session, jsonify, make_response
 
+
 app = Flask(__name__)
+
 app.json.compact = False
 
 app.secret_key = b'?w\x85Z\x08Q\xbdO\xb8\xa9\xb65Kj\xa9_'
@@ -23,7 +25,9 @@ def show_session(key):
 
     response.set_cookie('mouse', 'Cookie')
 
+
     return response
+
 
 if __name__ == '__main__':
     app.run(port=5555)
